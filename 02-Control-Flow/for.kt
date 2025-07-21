dee09202d0cbf7d6859c714bc7ef0d69a50517be
pavan0209 <pavanasonawane@gmail.com>
2025-07-21 15:18:13 +0530

    for loop

---
+++ 02-Control-Flow/for.kt
@@ -0,0 +1,14 @@
+// for loop in kotlin
+
+fun main() {
+
+    for (i in 1..5) {
+        print("$i ")
+    }
+    println()
+
+    for (i in 6 downTo 0 step 3) {
+        print("$i ")
+    }
+    println()
+}
