a5e2c0b6b902eaf579d0cee99bf50760f6f06e14
pavan0209 <pavanasonawane@gmail.com>
2025-07-22 15:51:09 +0530

    basic lambda function

---
+++ 04-Functions/10_lambda_functions.kt
@@ -0,0 +1,13 @@
+/*
+    Lambda Functions in Kotlin
+
+    Syntax:
+    val lambdaName: Type = { parameter(s) -> body }
+*/
+
+val greetings = { println("Hello, Pavan!") }
+
+fun main() {
+
+    greetings()
+}
