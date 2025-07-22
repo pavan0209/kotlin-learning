3c143d657e30b79fa9c276b721b8e353799669f8
pavan0209 <pavanasonawane@gmail.com>
2025-07-22 12:22:09 +0530

    function with return value and no parameters

---
+++ 04-Functions/03_return_function.kt
@@ -0,0 +1,12 @@
+// Function with return value and no parameters
+
+fun getName(): String {
+    return "Pavan"
+}
+
+fun main() {
+
+    val userName = getName()
+
+    println("Welcome, $userName")
+}
