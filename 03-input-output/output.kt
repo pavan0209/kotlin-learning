89b3d4c1ad3ec6e58ce341744f506060e1126acc
pavan0209 <pavanasonawane@gmail.com>
2025-07-22 11:06:25 +0530

    printing output

---
+++ 03-input-output/output.kt
@@ -0,0 +1,10 @@
+// Output in kotlin
+
+fun main(){
+
+    print("Hello, Kotlin");         // prints output on single line
+    println("\nHello, Kotlin ")     // print output and adds new line 
+
+    val age = 22
+    println("I am $age years old.") // String interpolation
+}
