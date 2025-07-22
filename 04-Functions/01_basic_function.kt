74bfaa4a04c69bc1b55878c106382eae3186f328
pavan0209 <pavanasonawane@gmail.com>
2025-07-22 12:12:41 +0530

    function without parameters and return value

---
+++ 04-Functions/01_basic_function.kt
@@ -0,0 +1,15 @@
+/*
+    Functions in Kotlin
+*/
+
+//  Function Without Parameters and Return Value
+fun run() {
+    println("In run function")
+}
+
+fun main() {
+
+    println("In main function")
+    run()                       // function call
+    println("In main function")
+}
